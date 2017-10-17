680948ca6173e2150b67bc51eb7876637fe20fd3
Steve05 <steve.yeh05@gmail.com>
2017-10-17 10:19:53 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SumStringsAsNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SumStringsAsNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("717e0588-d81c-4996-97d9-71a0bb5b760f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumStringsAsNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void input_123_456_should_return_579()
+        {
+            AssertSumStringsShouldBe("579", "123", "456");
+        }
+
+        [TestMethod]
+        public void input_N123_456_should_return_579()
+        {
+            AssertSumStringsShouldBe("333", "-123", "456");
+        }
+
+        [TestMethod]
+        public void input_empty_should_return_zero()
+        {
+            AssertSumStringsShouldBe("0", "", "");
+        }
+
+        private static void AssertSumStringsShouldBe(string expected, string strNum1, string strNum2)
+        {
+            var kata = new Kata();
+            var actual = kata.sumStrings(strNum1, strNum2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string sumStrings(string a, string b)
+        {
+            int.TryParse(a, out var num1);
+
+            int.TryParse(b, out var num2);
+
+            return (num1 + num2).ToString();
+        }
+
+    }
+}
